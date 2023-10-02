01bd9de0e33fa4ffb51080e642a0c7c729bc94f6
Hamid Kamyab <mr_kamyab@yahoo.com>
2023-10-02 15:43:29 +0330

    Complete sidebar

--- src/js/style.js
+++ src/js/style.js
@@ -3,3 +3,18 @@
     $('.icon-open', parent).toggleClass('open');
     $('.sub-menu', parent).slideToggle();
 });
+
+$('.sidebarBtn').click(function() {
+    $('.sidebar').toggleClass('close');
+
+    if ($('.sidebar').hasClass('close')) {
+        localStorage.setItem('menu-status', 'close')
+    } else {
+        localStorage.setItem('menu-status', 'open')
+    }
+})
+
+window.onload = () => {
+    var menuStatus = localStorage.getItem('menu-status');
+    $('.sidebar').addClass(menuStatus);
+}
